62733258fad9562dc032f1dd58088b86b6b388a1
Cliff L. Biffle <cliff@oxide.computer>
2020-02-17 10:39:34 -0800

    Polish (#4)
    * Cleanups in effects parser.
    
    * Use Default/From where appropriate
    
    * Allow trace to take a closure in addition to a raw fn
    
    * Remove unnecessary String reallocation on error paths
    
    * Promote Tockilator::err.
    
    I noticed that err doesn't actually use its &self parameter --
    unnecessary self-borrows are a thing that can tie you into knots, so I
    removed it.
    
    I then noticed that it's a general pattern that isn't specific to the
    Tockilator type, so I made it into a normal fn and simplified some
    parsing code.
    
    * Simplify error handling in loadobj.
    
    * Simplifications in loadobj_dwarf.
    
    * trace: error handling simplification
    
    * trace: ownership simplification
    
    * TockilatorState: prefer slice over &Vec
    
    Slice references are more general; the only time you'd really want to
    use &Vec is if the recipient needs to interrogate properties vectors
    have and slices don't, such as the allocated capacity.

--- src/tockilator.rs
+++ src/tockilator.rs
@@ -2,6 +2,7 @@
  * Copyright 2020 Oxide Computer Company
  */
 
+use std::borrow::Cow;
 use std::collections::BTreeMap;
 use std::convert::Infallible;
 use std::error::Error;
@@ -12,13 +13,13 @@ use std::io::{BufRead, BufReader};
 use std::str;
 
 use disc_v::*;
-use goblin::Object;
+use goblin::elf::Elf;
 use rustc_demangle::demangle;
 
 const TOCKILATOR_NREGS: usize = 32;
-const TOCKILATOR_REGPREFIX: char = 'x';
+const TOCKILATOR_REGPREFIX: &'static str = "x";
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Tockilator {
     symbols: BTreeMap<u64, (String, u64)>, // ELF symbols
     shortnames: BTreeMap<String, String>,  // demangled names from DWARF
@@ -30,7 +31,7 @@ pub struct Tockilator {
 pub struct TockilatorSymbol<'a> {
     pub addr: u32,
     pub name: &'a str,
-    pub demangled: &'a str,
+    pub demangled: Cow<'a, str>,
 }
 
 #[derive(Debug)]
@@ -44,7 +45,7 @@ pub struct TockilatorState<'a> {
     pub regs: &'a [u32; TOCKILATOR_NREGS],        // registers
     pub iasm: &'a str,    // instruction, as disp. by Verilator
     pub effects: &'a str, // effects, as disp. by Verilator
-    pub stack: &'a Vec<u32>, // stack
+    pub stack: &'a [u32], // stack
 }
 
 #[derive(Debug)]
@@ -60,11 +61,15 @@ pub enum TockilatorLoadobjOptions {
     None,
 }
 
-impl TockilatorError {
-    fn new(msg: &str) -> TockilatorError {
-        TockilatorError {
-            errmsg: msg.to_string(),
-        }
+impl<'a> From<&'a str> for TockilatorError {
+    fn from(msg: &'a str) -> TockilatorError {
+        msg.to_string().into()
+    }
+}
+
+impl From<String> for TockilatorError {
+    fn from(errmsg: String) -> TockilatorError {
+        TockilatorError { errmsg }
     }
 }
 
@@ -78,6 +83,10 @@ impl Error for TockilatorError {
     fn description(&self) -> &str {
         &self.errmsg
     }
+}
+
+fn err<S: ToString>(msg: S) -> Box<dyn Error> {
+    Box::new(TockilatorError::from(msg.to_string()))
 }
 
 /// Parse Ibex Trace output from Verilator into our state fields.
@@ -117,6 +126,9 @@ fn parse_verilator_line(
 fn parse_verilator_effects(
     effects: &str,
 ) -> Result<Option<(usize, u32)>, Box<dyn Error>> {
+    // The trace format includes register state changes, but it also includes
+    // bits of state printed as an FYI. The state changes use an equals sign,
+    // while the FYIs use a colon.
     let e: Vec<&str> = effects
         .split(" ")
         .filter(|eff| {
@@ -125,6 +137,8 @@ fn parse_verilator_effects(
             } else if eff.find("=").is_some() {
                 true
             } else {
+                // If we start getting effects that *aren't* = or :, we want to
+                // find out:
                 assert!(eff.find(":").is_some());
                 false
             }
@@ -136,63 +150,38 @@ fn parse_verilator_effects(
     }
 
     if e.len() > 1 {
-        return Err(Box::new(TockilatorError::new("too many effects")));
+        return Err(err("too many effects"));
     }
 
     let eff = e[0];
-    let eq = match eff.find("=0x") {
-        Some(eq) => eq,
-        None => {
-            return Err(Box::new(TockilatorError::new("bad effect value")));
-        }
-    };
+    let eq = eff.find("=0x").ok_or_else(|| err("bad effect value"))?;
 
-    if (&eff[..1]).chars().next().unwrap() != TOCKILATOR_REGPREFIX {
-        return Err(Box::new(TockilatorError::new("bad register name")));
+    if &eff[..1] != TOCKILATOR_REGPREFIX {
+        return Err(err("bad register name"));
     }
 
     let reg = u32::from_str_radix(&eff[1..eq], 10)? as usize;
     let val = u32::from_str_radix(&eff[eq + 1 + "0x".len()..], 0x10)?;
 
     if reg >= TOCKILATOR_NREGS {
-        return Err(Box::new(TockilatorError::new("invalid register")));
+        return Err(err("invalid register"));
     }
 
     Ok(Some((reg, val)))
 }
 
 impl Tockilator {
-    fn err<T>(&self, msg: &str) -> Result<T, Box<dyn Error>> {
-        Err(Box::new(TockilatorError::new(msg)))
-    }
-
-    pub fn new() -> Self {
-        Tockilator {
-            symbols: BTreeMap::new(),
-            shortnames: BTreeMap::new(),
-            regs: [0; TOCKILATOR_NREGS],
-            stack: vec![],
-        }
-    }
-
     pub fn loadobj(
         &mut self,
         obj: &str,
         options: TockilatorLoadobjOptions,
     ) -> Result<(), Box<dyn Error>> {
-        let buffer = match fs::read(obj) {
-            Err(err) => {
-                return self.err(&format!("failed to read: {}: {}", obj, err));
-            }
-            Ok(k) => k,
-        };
+        let buffer = fs::read(obj)
+            .map_err(|e| err(format!("failed to read: {}: {}", obj, e)))?;
 
-        let elf = match Object::parse(&buffer)? {
-            Object::Elf(e) => e,
-            _ => {
-                return self.err(&format!("unrecognized ELF object: {}", obj));
-            }
-        };
+        let elf = Elf::parse(&buffer).map_err(|e| {
+            err(format!("unrecognized ELF object: {}: {}", e, obj))
+        })?;
 
         match options {
             TockilatorLoadobjOptions::LoadDwarf => {
@@ -212,10 +201,10 @@ impl Tockilator {
             let name = match elf.strtab.get(sym.st_name) {
                 Some(n) => n?,
                 None => {
-                    return self.err(&format!(
+                    return Err(err(format!(
                         "bad symbol in object {}: {}",
                         obj, sym.st_name
-                    ));
+                    )));
                 }
             };
 
@@ -228,7 +217,7 @@ impl Tockilator {
 
     fn loadobj_dwarf(
         &mut self,
-        buffer: &Vec<u8>,
+        buffer: &[u8],
         elf: &goblin::elf::Elf,
     ) -> Result<(), Box<dyn Error>> {
         // Load all of the sections. This "load" operation just gets the data in
@@ -282,18 +271,15 @@ impl Tockilator {
                 while let Some(attr) = attrs.next()? {
                     match attr.name() {
                         gimli::constants::DW_AT_linkage_name => {
-                            linkage_name = Some(attr.value());
+                            linkage_name = dwarf_name(&dwarf, attr.value());
                         }
                         gimli::constants::DW_AT_name => {
-                            name = Some(attr.value());
+                            name = dwarf_name(&dwarf, attr.value());
                         }
                         _ => (),
                     }
                 }
-                if let (Some(nn), Some(ln)) = (
-                    self.dwarf_name(&dwarf, name),
-                    self.dwarf_name(&dwarf, linkage_name),
-                ) {
+                if let (Some(nn), Some(ln)) = (name, linkage_name) {
                     if ln != nn {
                         self.shortnames
                             .insert(String::from(ln), String::from(nn));
@@ -304,71 +290,37 @@ impl Tockilator {
         Ok(())
     }
 
-    fn dwarf_name<'a>(
-        &mut self,
-        dwarf: &'a gimli::Dwarf<gimli::EndianSlice<gimli::LittleEndian>>,
-        value: Option<
-            gimli::AttributeValue<gimli::EndianSlice<gimli::LittleEndian>>,
-        >,
-    ) -> Option<&'a str> {
-        match value? {
-            gimli::AttributeValue::DebugStrRef(strref) => {
-                if let Ok(dstring) = dwarf.debug_str.get_str(strref) {
-                    if let Ok(ddstring) = str::from_utf8(dstring.slice()) {
-                        return Some(ddstring);
-                    }
-                }
-            }
-            _ => (),
-        }
-        None
-    }
-
     fn trace(
         &mut self,
         source: &mut std::io::BufReader<std::fs::File>,
-        callback: fn(&TockilatorState) -> Result<(), Box<dyn Error>>,
+        callback: impl Fn(&TockilatorState) -> Result<(), Box<dyn Error>>,
     ) -> Result<(), Box<dyn Error>> {
-        let mut lineno = 2;
         let mut lines = source.lines();
 
-        let _header = match lines.next() {
-            Some(header) => header,
-            None => return self.err("zero-length input"),
-        };
+        // Expect, but discard, header line.
+        lines.next().ok_or_else(|| err("zero-length input"))??;
 
-        for line in lines {
-            let l = match line {
-                Ok(ll) => ll,
-                Err(_err) => {
-                    return self.err(&format!("I/O error on line {}", lineno));
-                }
-            };
+        // 1 to make lines 1-based, 1 to account for the header we skipped.
+        const OFFSET: usize = 2;
 
-            let res = match parse_verilator_line(&l) {
-                Some(res) => res,
-                None => {
-                    return self
-                        .err(&format!("invalid input on line {}", lineno));
-                }
-            };
+        for (lineno_zero, line) in lines.enumerate() {
+            let lineno = lineno_zero + OFFSET;
+            let l =
+                line.map_err(|_| err(format!("I/O error on line {}", lineno)))?;
+
+            let res = parse_verilator_line(&l).ok_or_else(|| {
+                err(format!("invalid input on line {}", lineno))
+            })?;
 
             let (time, cycle, pc, ibin, iasm, effects) = res;
 
-            match parse_verilator_effects(effects) {
-                Ok(val) => match val {
-                    None => {}
-                    Some(effect) => {
-                        self.regs[effect.0] = effect.1;
-                    }
-                },
-                Err(err) => {
-                    return self.err(&format!(
-                        "invalid effect on line {}: {}",
-                        lineno, err
-                    ));
-                }
-            };
+            let val = parse_verilator_effects(effects).map_err(|e| {
+                err(format!("invalid effect on line {}: {}", lineno, e))
+            })?;
+
+            if let Some((reg_no, value)) = val {
+                self.regs[reg_no] = value;
+            }
 
             /*
              * Now decode the instruction.
@@ -376,7 +328,6 @@ impl Tockilator {
             let inst = decode_inst(rv_isa::rv32, pc as u64, ibin as u64);
 
             let mut symbol: Option<TockilatorSymbol> = None;
-            let dem;
 
             if let Some(sym) = self.symbols.range(..=pc as u64).next_back() {
                 if (pc as u64) < *sym.0 + (sym.1).1 {
@@ -386,27 +337,26 @@ impl Tockilator {
                      * Check to see if we have a short name from DWARF;
                      * otherwise run it through the demangler.
                      */
-                    let sname =
+                    let demangled =
                         if let Some(shortname) = self.shortnames.get(name) {
-                            shortname
+                            shortname.into()
                         } else {
-                            dem = demangle(name).to_string();
-                            &dem
+                            demangle(name).to_string().into()
                         };
 
                     symbol = Some(TockilatorSymbol {
                         addr: *sym.0 as u32,
                         name: &(sym.1).0,
-                        demangled: sname,
+                        demangled,
                     });
                 }
             }
 
             callback(&TockilatorState {
-                line: lineno,
-                time: time,
-                cycle: cycle,
-                pc: pc,
+                line: lineno as u64,
+                time,
+                cycle,
+                pc,
                 symbol: symbol.as_ref(),
                 inst: &inst,
                 iasm: &iasm,
@@ -424,8 +374,6 @@ impl Tockilator {
                 }
                 _ => (),
             }
-
-            lineno += 1;
         }
 
         Ok(())
@@ -438,6 +386,20 @@ impl Tockilator {
     ) -> Result<(), Box<dyn Error>> {
         let mut file = BufReader::new(File::open(file)?);
         self.trace(&mut file, callback)
+    }
+}
+
+fn dwarf_name<'a>(
+    dwarf: &'a gimli::Dwarf<gimli::EndianSlice<gimli::LittleEndian>>,
+    value: gimli::AttributeValue<gimli::EndianSlice<gimli::LittleEndian>>,
+) -> Option<&'a str> {
+    match value {
+        gimli::AttributeValue::DebugStrRef(strref) => {
+            let dstring = dwarf.debug_str.get_str(strref).ok()?;
+            let ddstring = str::from_utf8(dstring.slice()).ok()?;
+            Some(ddstring)
+        }
+        _ => None,
     }
 }
 
